f437185d05de3dd1adb9e5f43912b37d9fbf7215
Gustavo Lima <gustavo.it@outlook.com>
2023-08-24 08:55:04 -0300

    testing azure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { GlobalStyle } from "./styles/globalTheme"
-import styled, {ThemeProvider} from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { SteamCardHover } from "./components/SteamCard";
 
 
@@ -14,28 +14,28 @@ const CardsContainer = styled.div`
   width: 100vw;
   height: 100vh;
 `
-  
+
 
 
 function App() {
 
   return (
-    <ThemeProvider theme={{color: 'black'}}>
+    <ThemeProvider theme={{ color: 'black' }}>
       <GlobalStyle />
       <CardsContainer>
-        <SteamCardHover 
+        <SteamCardHover
           imgUrl="https://images.unsplash.com/photo-1607796884038-3638822d5ee2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
         />
         <SteamCardHover
           imgUrl="https://images.unsplash.com/photo-1605899435973-ca2d1a8861cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
         />
-          <SteamCardHover
+        <SteamCardHover
           imgUrl="https://images.unsplash.com/photo-1536029058117-75fb5c1c4c46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=717&q=80"
         />
-          <SteamCardHover
-          imgUrl="https://images.unsplash.com/photo-1560419284-6c2d2b5e0483?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        <SteamCardHover
+          imgUrl="https://images.unsplash.com/photo-1565172265978-aaa872e3f618?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=627&q=80"
         />
-       
+
       </CardsContainer>
     </ThemeProvider>
   )
